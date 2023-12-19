b66ef3a899c80618218960fc62a86cda959eb45f
RokZerdoner <rok.zerdonern@gmail.com>
2023-12-19 14:51:41 +0100

    init commit

---
+++ app/src/main/java/um/feri/pora/easypermissionspora/MainActivity.kt
@@ -0,0 +1,11 @@
+package um.feri.pora.easypermissionspora
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
